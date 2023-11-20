d6343817a9fc6d7ec120ef966e2c678f0d3e25a9
Jesus David <jdrch62@gmail.com>
2023-11-20 17:00:29 -0600

    Avances del registro

--- src/features/auth/register-form/RegisterForm.tsx
+++ src/features/auth/register-form/RegisterForm.tsx
@@ -1,20 +1,61 @@
 import { FunctionComponent } from "react";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import useRegisterFormState from "./use-register-form-state";
 
 const RegisterForm: FunctionComponent<props> = ({disabled, onSubmit}) =>{
-    const {email} = useRegisterFormState();
+    const {name, email, password, confirmPassword, onChangeName, onChangeEmail, onChangePassword, onChangeConfirmPassword} = useRegisterFormState();
     return <>
+        <TextField
+            value={name}
+            label="Nombre"
+            type="name"
+            onChange={onChangeName}
+            disabled={disabled}
+
+        />
         <TextField
             value={email}
             label="Correo electronico"
+            type="email"
+            onChange={onChangeEmail}
+            disabled={disabled}
+
         />
+        <TextField
+            value={password}
+            label="Contraseña"
+            type="email"
+            onChange={onChangePassword}
+            disabled={disabled}
+
+        />
+        <TextField
+            value={confirmPassword}
+            label="Confirmar contraseña"
+            type="confirmPassword"
+            onChange={onChangeConfirmPassword}
+            disabled={disabled}
+
+        />
+        <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            disabled={disabled}
+            onClick={()=>{
+                onSubmit(name, email, password, confirmPassword);    
+            }}
+
+        >
+        Registrarse
+        </Button>
     </>
 }
 
 export interface props {
     disabled: boolean;
-    onSubmit: (email: string, password: string) => void;
+    onSubmit: (name: string, email: string, password: string, confirmPassword: string) => void;
 }
 
 export default RegisterForm;
